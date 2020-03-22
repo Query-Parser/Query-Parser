80e4b0e91c2181044e9d721c3eb82abfb536014e
Yaro <ysboyko@gmail.com>
2020-03-22 00:24:40 -0400

    In progess of sending textarea

--- restservice/src/main/java/com/example/restservice/QueryController.java
+++ restservice/src/main/java/com/example/restservice/QueryController.java
@@ -1,18 +1,20 @@
 package com.example.restservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import java.lang.String;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class QueryController {
 
-    private static final String query = "Null";
-
-    @GetMapping("/query")
-    public String query() {
-        return query;
+    @ResponseBody
+    @RequestMapping(value = "/query", method = RequestMethod.POST)
+    public String query(@RequestBody String resultQuery) {
+        return resultQuery;
     }
 }
