dc7669e8bb38c861ccd58c219f513e91f5ee0445
Yaro <ysboyko@gmail.com>
2020-03-22 00:33:38 -0400

    Adjusted links

--- restservice/src/main/java/com/example/restservice/controller/QueryController.java
+++ restservice/src/main/java/com/example/restservice/controller/QueryController.java
@@ -1,4 +1,4 @@
-package com.example.restservice;
+package com.example.restservice.controller;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
