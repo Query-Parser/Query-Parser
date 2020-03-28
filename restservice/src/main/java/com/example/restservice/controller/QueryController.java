c59ebeeceebf20f8b70818a947404876079bfe85
Yaro <ysboyko@gmail.com>
2020-03-28 13:54:54 -0400

    Added Databases Queries and Query Endpoint

--- restservice/src/main/java/com/example/restservice/controller/QueryController.java
+++ restservice/src/main/java/com/example/restservice/controller/QueryController.java
@@ -1,20 +1,37 @@
 package com.example.restservice.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import java.lang.String;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @CrossOrigin(origins = "*")
 @RestController
 public class QueryController {
 
+    private String template = "Null";
+    
     @ResponseBody
-    @RequestMapping(value = "/query", method = RequestMethod.POST)
-    public String query(@RequestBody String resultQuery) {
-        return resultQuery;
+    @PostMapping(value = "/query")
+    public String setQuery(@RequestBody String resultQuery) {
+        setTemplate(resultQuery);
+        return getTemplate();
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/query")
+    public String getQuery() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getTemplate() {
+        return template;
     }
 }
