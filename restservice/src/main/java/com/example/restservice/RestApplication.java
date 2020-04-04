4a59eb3265ab8b9dd2579478455fedc13bf54e5d
Jeannie <jeanniele2011@gmail.com>
2020-04-04 16:43:01 -0400

    Add method to get connection to all dbs

--- restservice/src/main/java/com/example/restservice/RestApplication.java
+++ restservice/src/main/java/com/example/restservice/RestApplication.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCursor;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
@@ -16,6 +25,9 @@ public class RestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
+
+	@Autowired
+	private MongoClient mongoClient;
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -27,6 +39,16 @@ public class RestApplication {
 			}
 		};
 	}
-	
+
+	public Map<String, MongoTemplate> getMongoTemplates() throws Exception {
+		MongoCursor<String> dbsCursor = mongoClient.listDatabaseNames().iterator();
+		Map<String, MongoTemplate> mongoTemplates = new HashMap<String, MongoTemplate>();
+		while(dbsCursor.hasNext()) {
+			String dbName = dbsCursor.next();
+			MongoDbFactory mongo = new SimpleMongoDbFactory(mongoClient, dbName);
+			mongoTemplates.put(dbName, new MongoTemplate(mongo));
+		}
+		return mongoTemplates;
+	}
 
 }
