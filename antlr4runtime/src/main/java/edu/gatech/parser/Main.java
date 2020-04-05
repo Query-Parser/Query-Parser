7a39333d4679e86c977d2f9d102429ad71d857e0
Hao Dang <hdang.1602@gmail.com>
2020-04-04 23:14:27 -0400

    add temporary db connection

--- antlr4runtime/src/main/java/edu/gatech/parser/Main.java
+++ antlr4runtime/src/main/java/edu/gatech/parser/Main.java
@@ -1,4 +1,26 @@
-package src.main.java.edu.gatech.parser;
+package edu.gatech.parser;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
 
 public class Main {
+    private static MongoDatabase db;
+    public static void main(String[] args){
+        System.out.println("hello world");
+        db = dbConfig();
+        
+    }
+
+    private static MongoDatabase dbConfig() {
+        MongoDatabase db = null;
+        MongoClientOptions.Builder option = MongoClientOptions.builder().serverSelectionTimeout(3600);
+        MongoCredential credential = MongoCredential.createCredential("root", "admin", "example".toCharArray());
+        MongoClient mongoClient = new MongoClient(new ServerAddress("timbess.net", 9200), credential, option.build());
+        db = mongoClient.getDatabase("test");
+
+        return db;
+    }
 }
