4101dbb45528658dc2c83bbd9103dca36900dbc2
Hao Dang <hdang.1602@gmail.com>
2020-04-05 11:02:05 -0400

    test run

--- antlr4runtime/src/main/java/edu/gatech/parser/Main.java
+++ antlr4runtime/src/main/java/edu/gatech/parser/Main.java
@@ -4,10 +4,18 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.antlr.v4.runtime.BufferedTokenStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static edu.gatech.parser.MySQLQueryParser.*;
 
 public class Main {
     private static MongoDatabase db;
@@ -19,28 +27,50 @@ public class Main {
         MySQLQueryLexer lexer = new MySQLQueryLexer(CharStreams.fromString(query));
         MySQLQueryParser parser = new MySQLQueryParser(new BufferedTokenStream(lexer));
         MySQLQueryBaseListener listener = new MySQLQueryBaseListener() {
+
+
             @Override
-            public void enterQuery(MySQLQueryParser.QueryContext ctx) {
+            public void enterQuery(QueryContext ctx) {
                 System.out.println("Enter Query");
             }
 
             @Override
-            public void exitQuery(MySQLQueryParser.QueryContext ctx) {
+            public void exitQuery(QueryContext ctx) {
                 System.out.println("Exit Query");
             }
 
             @Override
-            public void enterSelectStatement(MySQLQueryParser.SelectStatementContext ctx) {
+            public void enterSelectStatement(SelectStatementContext ctx) {
                 System.out.println("selecting from: ");
+
                 ctx.fromClause().tableList().tableItem().forEach((item) -> {
                     item.tableName().WORD().forEach(word -> System.out.println(word.getSymbol().getText()));
                 });
             }
 
             @Override
-            public void exitSelectStatement(MySQLQueryParser.SelectStatementContext cxt) {
+            public void exitSelectStatement(SelectStatementContext cxt) {
                 System.out.println("done selecting");
             }
+
+            @Override
+            public void enterFromClause(FromClauseContext ctx) {
+//                List<MongoCollection> collections = new ArrayList<>();
+                System.out.println("Enter From Clause");
+//
+//                for (TableItemContext item : ctx.tableList().tableItem()) {
+//                    for (TerminalNode tableName : item.tableName().WORD()) {
+//                        collections.add(db.getCollection(tableName.getSymbol().getText()));
+//                    }
+//                }
+            }
+
+            @Override
+            public void exitFromClause(FromClauseContext ctx) {
+                super.exitFromClause(ctx);
+            }
+
+
         };
         ParseTreeWalker.DEFAULT.walk(listener, parser.query());
     }
