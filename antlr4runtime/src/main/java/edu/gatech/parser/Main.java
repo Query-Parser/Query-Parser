00d7212e4db4a3d7260826c6e10a43e102cf7f6e
Hao Dang <hdang.1602@gmail.com>
2020-04-05 00:09:44 -0400

    walk the selected query

--- antlr4runtime/src/main/java/edu/gatech/parser/Main.java
+++ antlr4runtime/src/main/java/edu/gatech/parser/Main.java
@@ -5,13 +5,44 @@ import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Main {
     private static MongoDatabase db;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("hello world");
         db = dbConfig();
+        String query = "select * from foo, bar, baz"; // TODO: replace by args later
+        MySQLQueryLexer lexer = new MySQLQueryLexer(CharStreams.fromString(query));
+        MySQLQueryParser parser = new MySQLQueryParser(new BufferedTokenStream(lexer));
+        MySQLQueryBaseListener listener = new MySQLQueryBaseListener() {
+            @Override
+            public void enterQuery(MySQLQueryParser.QueryContext ctx) {
+                System.out.println("Enter Query");
+            }
 
+            @Override
+            public void exitQuery(MySQLQueryParser.QueryContext ctx) {
+                System.out.println("Exit Query");
+            }
+
+            @Override
+            public void enterSelectStatement(MySQLQueryParser.SelectStatementContext ctx) {
+                System.out.println("selecting from: ");
+                ctx.fromClause().tableList().tableItem().forEach((item) -> {
+                    item.tableName().WORD().forEach(word -> System.out.println(word.getSymbol().getText()));
+                });
+            }
+
+            @Override
+            public void exitSelectStatement(MySQLQueryParser.SelectStatementContext cxt) {
+                System.out.println("done selecting");
+            }
+        };
+        ParseTreeWalker.DEFAULT.walk(listener, parser.query());
     }
 
     private static MongoDatabase dbConfig() {
