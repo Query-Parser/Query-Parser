e2c897fad4027908bff2acc833c50e173ca54614
Hao Dang <hdang.1602@gmail.com>
2020-04-17 00:13:37 -0400

    * remove unused imports

--- antlr4runtime/src/main/java/edu/gatech/parser/Main.java
+++ antlr4runtime/src/main/java/edu/gatech/parser/Main.java
@@ -1,28 +1,13 @@
 package edu.gatech.parser;
 
-import com.mongodb.*;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import edu.gatech.db.DatabaseConfiguration;
-import org.antlr.v4.runtime.BufferedTokenStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.*;
-import org.bson.Document;
-
-import java.math.BigDecimal;
-import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.gatech.db.DatabaseConfiguration;
 
-import static edu.gatech.parser.MySQLQueryParser.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    private static MongoDatabase db = dbConfig();
     private static ObjectMapper mapper = new ObjectMapper();
     public static void main(String[] args) {
         String query = "select C._id as ci, E._id as ei from Customers C, Employees E limit 5;"; // TODO: replace by args later
@@ -37,15 +22,5 @@ public class Main {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-    }
-
-    private static MongoDatabase dbConfig() {
-        MongoDatabase db = null;
-        MongoClientOptions.Builder option = MongoClientOptions.builder().serverSelectionTimeout(3600);
-        MongoCredential credential = MongoCredential.createCredential("root", "admin", "example".toCharArray());
-        MongoClient mongoClient = new MongoClient(new ServerAddress("timbess.net", 9200), credential, option.build());
-        db = mongoClient.getDatabase("test");
-
-        return db;
     }
 }
