8d3901503271c818c70dd79ebac4bf354de2f099
Yaro <ysboyko@gmail.com>
2020-04-19 01:43:48 -0400

    Adjusting Query Box issue

--- restservice/src/main/java/com/example/restservice/controller/QueryController.java
+++ restservice/src/main/java/com/example/restservice/controller/QueryController.java
@@ -19,29 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class QueryController {
 
-    private String template = "Null";
-
-    @ResponseBody
-    @PostMapping(value = "/query")
-    public String setQuery(@RequestBody String resultQuery) {
-        setTemplate(resultQuery);
-        return getTemplate();
-    }
-
    @ResponseBody
-   @GetMapping(value = "/query")
-   public Map<String, List<Map<String, Object>>> getQuery() {
+   @PostMapping(value = "/query")
+   public Map<String, List<Map<String, Object>>> getQuery(@RequestBody String resultQuery) {
        System.out.println("getting resulting data");
        DatabaseConfiguration dbConfig = new DatabaseConfiguration();
        SQLEngine sqlEngine = new SQLEngine(dbConfig);
-       return sqlEngine.execute(getTemplate());
+       return sqlEngine.execute(resultQuery);
    }
-
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    public String getTemplate() {
-        return template;
-    }
 }
